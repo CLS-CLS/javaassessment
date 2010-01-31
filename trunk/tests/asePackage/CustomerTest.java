c8ed0a98f3fcede78a38cddd9e177914051baca3
ChrisLytsikas <ChrisLytsikas@40c025a2-0862-11df-9339-7f1b9139ac79>
2010-01-31 20:27:05 +0000

    //changed customer id from type int to double (just for testing) .. alse changed the Jtest in order to complie with the above change

--- trunk/tests/asePackage/CustomerTest.java
+++ trunk/tests/asePackage/CustomerTest.java
@@ -12,7 +12,9 @@ public class CustomerTest {
 	public void customer(){
 		Customer c1 = new Customer(1,"Christos Lytsikas");
 		assertEquals("Christos Lytsikas", c1.getFullName());
-		assertEquals(1, c1.getId());
+		assertEquals(1, c1.getId(), 0);
+		
+		
 	}
 }
 
