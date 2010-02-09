e9a327caf1e56c070331934fb84be67249d314c9
jcryeo <jcryeo@40c025a2-0862-11df-9339-7f1b9139ac79>
2010-02-09 22:13:04 +0000

    The account class with several improvements. New methods: - getId() - toString() - equals and hash
    Also the balance is saved as long to permit bigger values

--- src/asePackage/Account.java
+++ src/asePackage/Account.java
@@ -1,11 +1,62 @@
 package asePackage;
 
-public class Account {
+import java.util.ArrayList;
 
+public class Account {
+	private int accountID;
+	private ArrayList <Customer> ownerList;
+	private long balance;
+
+	public Account (int accountID) {        
+		this.accountID = accountID;
+		this.ownerList = new ArrayList<Customer> ();
+		this.balance = 0;
+	}
 	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return accountID;
+	}
+	public ArrayList<Customer> getOwnerList() {
+		return ownerList;
+	}
+	public long getBalance() {
+		return balance;
+	}
+	/*
+	 * public boolean testOwnership(Customer owner) {
+	 * TODO
+	 * }
+	 */
+	public void withdrawMoney(long sum) {
+		this.balance-=sum;
+	}
+	public void depositMoney(long sum) {
+		this.balance+=sum;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [ID=" + accountID + ", Balance=" + balance + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + accountID;
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (accountID != other.accountID)
+			return false;
+		return true;
+	}	
 }
