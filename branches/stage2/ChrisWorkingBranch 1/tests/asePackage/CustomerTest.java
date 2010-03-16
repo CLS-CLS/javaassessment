48a44b933c0e27decb623c59fd3ac2d5a0781ca9
ChrisLytsikas <ChrisLytsikas@40c025a2-0862-11df-9339-7f1b9139ac79>
2010-03-16 15:30:49 +0000

    My working space for stage 2

---
+++ branches/stage2/ChrisWorkingBranch 1/tests/asePackage/CustomerTest.java
@@ -0,0 +1,44 @@
+package asePackage;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * 
+ * @author Ali
+ *
+ */
+
+public class CustomerTest 
+{
+
+/**
+ * tests the constructor, getters and setters
+ */
+	@Test
+	public void customer()
+	{
+		Customer c1 = new Customer("Ali", "Alshbel", 1);
+		assertEquals("Ali", c1.getFirstName());
+		assertEquals("Alshbel", c1.getLastName());
+		assertEquals(1, c1.getId());
+		c1.setFirstName("Monica");
+		c1.setLastName("Farrow");
+		c1.setId(2);
+		assertEquals("Monica", c1.getFirstName());
+		assertEquals("Farrow", c1.getLastName());
+		
+		assertEquals(2, c1.getId());
+	}
+	
+	@Test
+	public void equals()
+	{
+		Customer c1 = new Customer("Ali", "Alshbel", 2);
+		Customer c2 = new Customer("Monica","Alshbel",1);
+		Customer c3 = new Customer("Ali", "Alshbel", 2);
+		assertTrue(c1.equals(c1));
+		assertFalse(c1.equals(c2));
+		assertTrue(c1.equals(c3));
+	}
+}
---
+++ branches/stage2/ChrisWorkingBranch 1/tests/asePackage/QueueTest.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package asePackage;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Ioan
+ *
+ */
+public class QueueTest {
+	private Queue queue1;
+	private Queue queue2;
+	private Queue queue3;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		ArrayList<Transaction> transactionTest=new ArrayList<Transaction>();
+		transactionTest.add(new Transaction("deposit", new Account(1), 150));
+		transactionTest.add(new Transaction("withdrawal", new Account(2), 100));
+		
+		queue1=new Queue();
+		queue2=new Queue(new Customer("Ioan", "Eu", 1),new Transaction("deposit", new Account(2), 100),1);
+		queue3=new Queue(new Customer("Ioan", "Eu", 1),transactionTest,2);
+	}
+
+	/**
+	 * Test method for {@link asePackage.Queue#Queue()}.
+	 */
+	@Test
+	public void testQueue1() {
+		Integer result=0;
+		Integer currentValue=queue1.getQueueNumber();
+		assertTrue("Wrong Output (Is: "+currentValue+"; Should be "+result+")",currentValue.equals(result));
+	}
+
+	/**
+	 * Test method for {@link asePackage.Queue#Queue(asePackage.Customer, asePackage.Transaction, int)}.
+	 */
+	@Test
+	public void testQueue2() {
+		Integer result=1;
+		Integer currentValue=queue2.getQueueNumber();
+		assertTrue("Wrong Output (Is: "+currentValue+"; Should be "+result+")",currentValue.equals(result));
+	}
+
+	/**
+	 * Test method for {@link asePackage.Queue#Queue(asePackage.Customer, java.util.ArrayList, int)}.
+	 */
+	@Test
+	public void testQueue3() {
+		Integer result=2;
+		Integer currentValue=queue3.getQueueNumber();
+		assertTrue("Wrong Output (Is: "+currentValue+"; Should be "+result+")",currentValue.equals(result));
+	}
+
+	/**
+	 * Test method for {@link asePackage.Queue#getCustomer()}.
+	 */
+	@Test
+	public void testGetCustomer() {
+		String result="Ioan";
+		String currentValue=""+queue2.getCustomer().getFirstName();
+		assertTrue("Wrong first name (Is: "+currentValue+"; Should be "+result+")",currentValue.equals(result));
+	}
+
+	/**
+	 * Test method for {@link asePackage.Queue#getTransactionList()}.
+	 */
+	@Test
+	public void testGetTransactionList() {
+		Integer result=2;
+		Integer currentValue=queue3.getTransactionList().size();
+		assertTrue("Wrong transactions number (Is: "+currentValue+"; Should be "+result+")",currentValue.equals(result));
+	}
+
+	/**
+	 * Test method for {@link asePackage.Queue#getQueueNumber()}.
+	 */
+	@Test
+	public void testGetQueueNumber() {
+		Integer result=1;
+		Integer currentValue=queue2.getQueueNumber();
+		assertTrue("Wrong number (Is: "+currentValue+"; Should be "+result+")",currentValue.equals(result));
+	}
+
+}
